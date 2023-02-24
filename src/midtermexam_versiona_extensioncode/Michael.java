da90c1eddcb55fdb4e2d39a96a3260b9bf25b86b
Michael Sousa <sousa8@sheridancollege.ca>
2023-02-24 12:23:38 -0500

    Created new brnach and new class

---
+++ src/midtermexam_versiona_extensioncode/Michael.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author XeroS
+ */
+public class Michael {
+    //Added _sousa_michael branch Michael class to that branch
+}
